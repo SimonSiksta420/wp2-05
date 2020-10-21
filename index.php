67f6635870be4643dff41867c7d651d2cbbb8f70
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-10-21 08:35:53 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>závod</title>
+</head>
+<body>
+<?php
+
+$students = array(
+  '1' => "Martin Lédl",
+  '2' => "Sorin Eni",
+  '3' => "Milan Ngo",
+  '4' => "Michal Plaček",
+  '5' => "Štěpán Kolarovský",
+  '6' => "Jakub Vávrů",
+  '7' => "Honza Hladík",
+  '8' => "Jan Šlechta",
+  '9' => "Karel Nakládal",
+  '10' => "Mao Ce Tung",
+  
+);
+
+?>
+
+<H1> Závod </H1>
+
+<?php
+for ($i = $students; $i < 10 ; $i++) { 
+    $students
+}
+
+?>
+
+</body>
+</html>
